5c1e6de9bea1a24fe37236646b637642c46227d5
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-03-28 09:54:30 -0700

    done

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,25 @@
-// Code SimplerComponent Here
+import React from 'react';
+
+// class SimplerComponent extends React.Component {
+//     constructor(props) {
+//         super(props)
+//     }
+
+    // handleClick = (e) => {
+    //     console.log(e)
+    // }
+    
+//     render() {
+//         return (
+//             <div onClick={this.handleClick}>I am just happy</div>
+//         )
+//     }
+// }
+
+// handleClick = e => {
+//     console.log("I am just happy")
+// }
+
+const SimplerComponent = (props) => <div onClick={props.handleClick}>I am just happy</div>
+
+export default SimplerComponent
